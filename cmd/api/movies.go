cfe5efe9e673e30925145bbfe199ff5b8727496d
Matej Topolovac <>
2024-08-25 17:32:57 +0200

    feat: added error handling

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 	"time"
 
@@ -9,7 +8,21 @@ import (
 )
 
 func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "create a new movie")
+	var input struct {
+		Title   string   `json:"title"`
+		Year    int32    `json:"year"`
+		Runtime int32    `json:"runtime"`
+		Genres  []string `json:"genres"`
+	}
+
+	err := app.readJSON(r, &input)
+
+	if err != nil {
+		app.badRequestResponse(w, r, err)
+		return
+	}
+
+	app.writeJSON(w, http.StatusAccepted, envelope{"movie": input}, nil)
 }
 
 func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request) {
